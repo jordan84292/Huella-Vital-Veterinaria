830835ebc9566b3934b19063c3006cd6194b615e
Jordan Gonzalez <gonzalezjordan61@gmail.com>
2026-01-30 20:00:20 -0600

    Se agregar la api de render

--- app/axiosApi/axiosApi.ts
+++ app/axiosApi/axiosApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosApi = axios.create({
-  baseURL: "https://vercel.jgonzproductions.com/api/v1",
+  baseURL: "https://api-huella-vital.onrender.com/api/v1",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
